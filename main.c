cd99c333d92bb5f29b4ab7a88c6258b86f929adb
Bernardo Robaina <bernardo.robaina@serel.com.br>
2025-05-17 20:17:35 -0300

    Version that passed all three test cases

--- main.c
+++ main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
+#include <stdlib.h>
 
-bool verificaAdjacentes(char board[3][4], int i, int j, int linhas, int colunas, int wordIndex, char* word){
+bool verificaAdjacentes(char board[3][4], int i, int j, int linhas, int colunas, int wordIndex, char* word, bool **visitados){
+    if(visitados[i][j] == true){
+        printf("Já foi visitado, retornando falso");
+        return false;
+    }
     //Se chegou no final da palavra
     if(wordIndex == strlen(word)) return true;
     
@@ -10,37 +15,54 @@ bool verificaAdjacentes(char board[3][4], int i, int j, int linhas, int colunas,
     printf("Posição atual: [%d][%d] Letra: %c\n", i, j, board[i][j]);
     printf("Procurando letra: %c\n", word[wordIndex]);
 
+
     bool achou = false;
 
     if((i+1 < linhas) && board[i+1][j] == word[wordIndex]){
+        visitados[i][j] = true;
         printf("%c", board[i+1][j]);
-        achou = verificaAdjacentes(board, i+1, j, linhas, colunas,(wordIndex + 1), word);
+        achou = verificaAdjacentes(board, i+1, j, linhas, colunas,(wordIndex + 1), word, visitados);
     }
     if((j+1 < colunas) && achou == false && board[i][j+1] == word[wordIndex]){
+        visitados[i][j] = true;
         printf("%c", board[i][j+1]);
-        achou = verificaAdjacentes(board, i, j+1, linhas, colunas,(wordIndex + 1), word);
+        achou = verificaAdjacentes(board, i, j+1, linhas, colunas,(wordIndex + 1), word, visitados);
     }
     if((i-1 >=0) && achou == false && board[i-1][j] == word[wordIndex]){
+        visitados[i][j] = true;
         printf("%c", board[i-1][j]);
-        achou = verificaAdjacentes(board, i-1, j, linhas, colunas,(wordIndex + 1), word);
+        achou = verificaAdjacentes(board, i-1, j, linhas, colunas,(wordIndex + 1), word, visitados);
     }
     if((j-1 >=0) && achou == false && board[i][j-1] == word[wordIndex]){
+        visitados[i][j] = true;
         printf("%c", board[i][j-1]);
-        achou = verificaAdjacentes(board, i, j-1, linhas, colunas,(wordIndex + 1), word);
+        achou = verificaAdjacentes(board, i, j-1, linhas, colunas,(wordIndex + 1), word, visitados);
     }
+
+    visitados[i][j] = false;
 
     return achou;
 }
 
 bool exist(char board[3][4], int boardSize, int boardColSize, char* word) {
     bool result = false;
-    int wordIndex = 0;
+
+    // matriz de visitados (para nao verificar a mesma célula duas vezes)
+    bool** visitados = malloc(boardSize * sizeof(bool*));
+    for (int i = 0; i < boardSize; i++) {
+        visitados[i] = calloc(boardColSize, sizeof(bool));
+    }
 
     for(int i = 0; i < boardSize; i++){
         for(int j = 0; j < boardColSize; j++){
             if(board[i][j] == word[0]){
-                result = verificaAdjacentes(board, i, j, boardSize, boardColSize, 1, word);
+                result = verificaAdjacentes(board, i, j, boardSize, boardColSize, 1, word, visitados);
                 if(result == true){
+                    for(int k = 0; k < boardSize; k++){
+                        free(visitados[k]);
+                    }
+                    free(visitados);
+
                     return result;
                 }
             }
@@ -58,8 +80,8 @@ void main(){
     bool existe = exist(matrix, 3, 4, word);
 
     if(existe){
-        printf("Existe");
+        printf("\nExiste");
     }else{
-        printf("Não existe");
+        printf("\nNão existe");
     }
 }
