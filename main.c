12c043d1f026286a3bba9ffbba6c92cdcc7c6135
Bernardo Robaina <bernardo.robaina@serel.com.br>
2025-05-17 14:08:18 -0300

    First version that worked(at least for the first case)

--- main.c
+++ main.c
@@ -1,16 +1,67 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 
-bool exist(char** board, int boardSize, int* boardColSize, char* word) {
+bool verificaAdjacentes(char board[3][4], int i, int j, int wordIndex, char* word){
+    printf("\nTesting:\n");
+    printf("Current position: [%d][%d]: %c\n", i, j, board[i][j]);
+    printf("Current word letter: %c\n", word[wordIndex]);
+
+    //Se tentar acessar um índice maior que a palavra quer dizer que ele encontrou a palavra
+    if(wordIndex == strlen(word)){
+        return true;
+    }
+
+    bool achou = false;
+
+    if(board[i+1][j] == word[wordIndex]){
+        printf("%c", board[i+1][j]);
+        achou = verificaAdjacentes(board, i+1, j, (wordIndex + 1), word);
+    }
+    if(board[i][j+1] == word[wordIndex]){
+        printf("%c", board[i][j+1]);
+        achou = verificaAdjacentes(board, i, j+1, (wordIndex + 1), word);
+    }
+    if(board[i-1][j] == word[wordIndex]){
+        printf("%c", board[i-1][j]);
+        achou = verificaAdjacentes(board, i-1, j, (wordIndex + 1), word);
+    }
+    if(board[i][j-1] == word[wordIndex]){
+        printf("%c", board[i][j-1]);
+        achou = verificaAdjacentes(board, i, j-1, (wordIndex + 1), word);
+    }
+
+    return achou;
+}
+
+bool exist(char board[3][4], int boardSize, int boardColSize, char* word) {
     bool result = false;
+    int wordIndex = 0;
 
-    return result
+    for(int i = 0; i < boardSize; i++){
+        for(int j = 0; j < boardColSize; j++){
+            if(board[i][j] == word[0]){
+                result = verificaAdjacentes(board, i, j, 1, word);
+                if(result == true){
+                    return result;
+                }
+            }
+        }
+    }
+
+    return result;
 }
 
 void main(){
     char matrix[3][4] = {{'A','B','C','E'},{'S','F','C','S'},{'A','D','E','E'}};
     int matrixSize = sizeof(matrix) / sizeof(char);
-    char word = "ABCCED";
+    char* word = "ABCCED";
 
-    bool existe = exist(matrix, matrixSize, 4, word);
+    bool existe = exist(matrix, 3, 4, word);
+
+    if(existe){
+        printf("Existe");
+    }else{
+        printf("Não existe");
+    }
 }
